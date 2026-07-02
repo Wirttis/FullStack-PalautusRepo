c683ec0bc57ad1f4b8fc8fe55f4a18301a3ddee3
Wirttis <95577610+Wirttis@users.noreply.github.com>
2026-07-02 23:19:42 +0300

    tehtavat 1.1-1.5

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,63 @@
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course = {course} />
+      <Content parts = {course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>    
+  </div>
+  )
+}
+const Content = ({parts}) => {
+  console.log(parts)
+  return (
+    <div>
+      <Part parts={parts[0]} />
+      <Part parts={parts[1]} />
+      <Part parts={parts[2]} />      
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>    
+    </div>
+    
+  )
+}
+
+const Part = ({parts}) => {
+  return (
+    <div>
+      <p>{parts.name} {parts.exercises}</p>    
+    </div>
+  )
+}
+
+export default App
